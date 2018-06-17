d5c7e8f266ccd96387a50ba4de534d63502c0d48
Rakesh Kura <krakeshk17@gmail.com>
2018-06-17 18:00:57 -0400

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'praveen-ineventory',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-54-152-103-51.compute-1.amazonaws.com',
+        key: '~/.ssh/ec2-inventory.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:rockkura/praveen-inventory.git',
+        path: '/home/ubuntu/praveen-inventory',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
